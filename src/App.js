621f2fe4c1982d45aff6fe0aa125604da9921690
leen <leenodeh287@gmail.com>
2023-11-04 15:26:27 +0300

    add all pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './Pages/Home/Home.jsx'
+import Create from './Pages/Create/Create.jsx'
+import Header from "./Component/Header.jsx";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/create" element={<Create/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Create/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Create.scss'
+
+const Create = () => {
+  return (
+    <div>
+      create
+    </div>
+  )
+}
+
+export default Create
