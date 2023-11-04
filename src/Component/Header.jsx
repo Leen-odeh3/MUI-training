370a5fe2a25b7f6bf58163c84b0ece9c2bbeaa14
leen <leenodeh287@gmail.com>
2023-11-04 20:08:56 +0300

    AppBar component

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import React from "react";
+
+import { Drawer, Divider } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
 const Header = () => {
-  return (
-    <div>
-      header
-    </div>
-  )
-}
+  const drawerWidth = 260;
 
-export default Header
+  return (
+    <Box>
+      <AppBar position="static" sx={{marginLeft:`${drawerWidth}px`}}  >
+        <Toolbar>
+          <Typography sx={{flexGrow:"1"}}>My Expenses</Typography>
+          <Typography variant="h5">Leen</Typography>
+          <Avatar sx={{ bgcolor: "gray",marginLeft:"10px" }} variant="rounded">L</Avatar>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Header;
