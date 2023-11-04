8ec903962754d2171c69b2751de08dc647edd695
leen <leenodeh287@gmail.com>
2023-11-04 20:28:34 +0300

    add ListIcon Buttons

--- src/Component/Drawar.jsx
+++ src/Component/Drawar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Drawer, Divider,Toolbar } from "@mui/material";
-
+import { Drawer, Divider,Toolbar, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import CreateIcon from '@mui/icons-material/Create';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useNavigate } from 'react-router-dom';
 
 const Drawar = () => {
+    const navigate=useNavigate();
     const drawerWidth = 260;
 
   return (
@@ -20,6 +26,40 @@ const Drawar = () => {
   >
     <Toolbar />
     <Divider />
+
+<List>
+    <ListItem>
+        <ListItemButton onClick={()=>navigate("/home")}>
+            <ListItemIcon><HomeIcon/></ListItemIcon>
+            <ListItemText>Home</ListItemText>
+        </ListItemButton>
+        </ListItem>
+        <ListItem>
+        <ListItemButton onClick={()=>navigate("/create")}>
+            <ListItemIcon><CreateIcon/>   </ListItemIcon>
+            <ListItemText>Create</ListItemText>
+        </ListItemButton>
+        </ListItem>
+        <ListItem>
+        <ListItemButton onClick={()=>navigate("/settings")}>
+            <ListItemIcon> <SettingsIcon/>  </ListItemIcon>
+            <ListItemText>Settings</ListItemText>
+        </ListItemButton>
+        </ListItem>
+        <ListItem>
+        <ListItemButton onClick={()=>navigate("/profile")}>
+            <ListItemIcon> <AccountCircleIcon/> </ListItemIcon>
+            <ListItemText>Profile</ListItemText>
+        </ListItemButton>
+    </ListItem>
+    <ListItem>
+        <ListItemButton onClick={()=>navigate("/logout")}>
+            <ListItemIcon> <ExitToAppIcon/> </ListItemIcon>
+            <ListItemText>Log Out</ListItemText>
+        </ListItemButton>
+    </ListItem>
+</List>
+
   </Drawer>
   )
 }
